41e6804f88728826e72c2b5f0e94a4a679fa6b9f
Joseph Suvak <robert.suvak@hcahealthcare.com>
2021-07-06 20:04:00 -0500

    Add Centralized Meta
    Inserts Meta into Vue Header

--- src/components/Jedi.vue
+++ src/components/Jedi.vue
@@ -36,6 +36,18 @@ export default {
   name: 'Jedi',
   props: {
     msg: String
+  },
+  metaInfo: {
+    title: 'Jedi Temple',
+    titleTemplate: '%s | Overview of The Order',
+    htmlAttrs: {
+      lang: 'en-US'
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'description', content: 'An landing page for all you want to know Jedi.' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' }
+    ]
   }
 }
 </script>
